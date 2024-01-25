81de0cf5be3c5c0d311d2ff5269d651f07a3610c
zhigeng <84454978+cancanneedcoding@users.noreply.github.com>
2024-01-25 13:47:49 +0800

    SegmentTree Template (#2)
    * 上传SegmentTree1.cpp
    
    * Delete 数据结构 directory
    
    * Update SegmentTree1.cpp
    
    * 上传树链剖分
    
    * Update HLD.cpp
    
    * 上传SegmentTree2
    
    * Update SegmentTree2.cpp
    
    * 上传LazySegmentTree1.cpp
    
    * Update LazySegmentTree1.cpp
    
    * Update LazySegmentTree1.cpp
    
    * 上传LazySegmentTree2
    
    * Update LazySegmentTree2.cpp
    
    * 上传并查集和树状数组
    
    * Update DSU.cpp
    
    * Update Fenwick.cpp
    
    * Update HLD.cpp
    
    * Update SegmentTree1.cpp

---
+++ DataStructure/LazySegmentTree1.cpp
@@ -0,0 +1,145 @@
+/*
+懒标记线段树
+模版来源：jiangly
+支持功能：单点修改，区间加，区间查询
+*/
+template <class Info, class Tag>
+struct LazySegmentTree
+{
+    const int n;
+    std::vector<Info> info;
+    std::vector<Tag> tag;
+    LazySegmentTree(int n) : n(n), info(4 << (int)std::log2(n)), tag(4 << (int)std::log2(n)) {}
+    LazySegmentTree(std::vector<Info> init) : LazySegmentTree(init.size())
+    {
+        std::function<void(int, int, int)> build = [&](int p, int l, int r)
+        {
+            if (r - l == 1)
+            {
+                info[p] = init[l];
+                return;
+            }
+            int m = (l + r) / 2;
+            build(2 * p, l, m);
+            build(2 * p + 1, m, r);
+            pull(p);
+        };
+        build(1, 0, n);
+    }
+    void pull(int p)
+    {
+        info[p] = info[2 * p] + info[2 * p + 1];
+    }
+    void apply(int p, const Tag &v)
+    {
+        info[p].apply(v);
+        tag[p].apply(v);
+    }
+    void push(int p)
+    {
+        apply(2 * p, tag[p]);
+        apply(2 * p + 1, tag[p]);
+        tag[p] = Tag();
+    }
+    void modify(int p, int l, int r, int x, const Info &v)
+    {
+        if (r - l == 1)
+        {
+            info[p] = v;
+            return;
+        }
+        int m = (l + r) / 2;
+        push(p);
+        if (x < m)
+        {
+            modify(2 * p, l, m, x, v);
+        }
+        else
+        {
+            modify(2 * p + 1, m, r, x, v);
+        }
+        pull(p);
+    }
+    void modify(int p, const Info &v)
+    {
+        modify(1, 0, n, p, v);
+    }
+    Info rangeQuery(int p, int l, int r, int x, int y)
+    {
+        if (l >= y || r <= x)
+        {
+            return Info();
+        }
+        if (l >= x && r <= y)
+        {
+            return info[p];
+        }
+        int m = (l + r) / 2;
+        push(p);
+        return rangeQuery(2 * p, l, m, x, y) + rangeQuery(2 * p + 1, m, r, x, y);
+    }
+    Info rangeQuery(int l, int r)
+    {
+        return rangeQuery(1, 0, n, l, r);
+    }
+    void rangeApply(int p, int l, int r, int x, int y, const Tag &v)
+    {
+        if (l >= y || r <= x)
+        {
+            return;
+        }
+        if (l >= x && r <= y)
+        {
+            apply(p, v);
+            return;
+        }
+        int m = (l + r) / 2;
+        push(p);
+        rangeApply(2 * p, l, m, x, y, v);
+        rangeApply(2 * p + 1, m, r, x, y, v);
+        pull(p);
+    }
+    void rangeApply(int l, int r, const Tag &v)
+    {
+        return rangeApply(1, 0, n, l, r, v);
+    }
+};
+
+constexpr ll inf = 1E18;
+
+// 标记
+struct Tag
+{
+    ll add;
+    Tag(int _rev = 0) : add{_rev} {}
+    // 标记更新
+    void apply(const Tag &t)
+    {
+        add += t.add;
+    }
+};
+// 节点信息
+struct Info
+{
+    int len;       // 区间长度
+    long long sum; // 区间和
+
+    // 默认构造函数
+    Info() : len(1), sum(0) {}
+
+    // 两参数构造函数，用于设置区间和和区间长度
+    Info(long long v, int x) : len(x), sum(v) {}
+
+    // 信息更新函数，根据标记t来更新当前信息
+    void apply(const Tag &t)
+    {
+        sum += t.add * len; // 更新区间和，将标记的增加值乘以区间长度
+    }
+};
+Info operator+(const Info &a, const Info &b)
+{
+    Info c;
+    c.len = a.len + b.len;
+    c.sum = a.sum + b.sum;
+    return c;
+}
---
+++ DataStructure/SegmentTree1.cpp
@@ -0,0 +1,233 @@
+#define ll long long
+/*
+线段树
+维护信息：最大值,最小值,总和
+特点：支持三个属性的线段树上二分
+是否支持修改：支持单点修改
+*/
+class SegmentTree
+{
+private:
+    struct Node
+    {
+        ll maxVal;
+        ll minVal;
+        ll sum;
+    };
+
+    vector<Node> tree;
+    int n;
+
+    void buildTree(const vector<int> &nums, int treeIndex, int lo, int hi)
+    {
+        if (lo == hi)
+        {
+            tree[treeIndex] = {nums[lo], nums[lo], nums[lo]};
+            return;
+        }
+
+        int mid = lo + (hi - lo) / 2;
+        int leftChild = 2 * treeIndex + 1;
+        int rightChild = 2 * treeIndex + 2;
+
+        buildTree(nums, leftChild, lo, mid);
+        buildTree(nums, rightChild, mid + 1, hi);
+
+        tree[treeIndex].maxVal =
+            max(tree[leftChild].maxVal, tree[rightChild].maxVal);
+        tree[treeIndex].minVal =
+            min(tree[leftChild].minVal, tree[rightChild].minVal);
+        tree[treeIndex].sum = tree[leftChild].sum + tree[rightChild].sum;
+    }
+
+    void updateTree(int treeIndex, int lo, int hi, int arrIndex, ll val)
+    {
+        if (lo == hi)
+        {
+            tree[treeIndex] = {val, val, val};
+            return;
+        }
+
+        int mid = lo + (hi - lo) / 2;
+        int leftChild = 2 * treeIndex + 1;
+        int rightChild = 2 * treeIndex + 2;
+
+        if (arrIndex <= mid)
+        {
+            updateTree(leftChild, lo, mid, arrIndex, val);
+        }
+        else
+        {
+            updateTree(rightChild, mid + 1, hi, arrIndex, val);
+        }
+
+        tree[treeIndex].maxVal =
+            max(tree[leftChild].maxVal, tree[rightChild].maxVal);
+        tree[treeIndex].minVal =
+            min(tree[leftChild].minVal, tree[rightChild].minVal);
+        tree[treeIndex].sum = tree[leftChild].sum + tree[rightChild].sum;
+    }
+
+    Node queryTree(int treeIndex, int lo, int hi, int i, int j)
+    {
+        if (lo > j || hi < i)
+            return {INT_MIN, INT_MAX, 0}; // Out of range
+        if (i <= lo && hi <= j)
+            return tree[treeIndex]; // Segment is completely within range
+
+        int mid = lo + (hi - lo) / 2;
+        int leftChild = 2 * treeIndex + 1;
+        int rightChild = 2 * treeIndex + 2;
+
+        Node leftQuery = queryTree(leftChild, lo, mid, i, j);
+        Node rightQuery = queryTree(rightChild, mid + 1, hi, i, j);
+
+        // Merge left and right query results
+        return {max(leftQuery.maxVal, rightQuery.maxVal),
+                min(leftQuery.minVal, rightQuery.minVal),
+                leftQuery.sum + rightQuery.sum};
+    }
+
+    ll binarySearchSum(int treeIndex, int curLo, int curHi, ll target,
+                       ll &acc)
+    {
+        if (curLo == curHi)
+        {
+            if (acc + tree[treeIndex].sum >= target)
+                return curLo;
+            acc += tree[treeIndex].sum;
+            return -1;
+        }
+
+        int mid = curLo + (curHi - curLo) / 2;
+        int leftChild = 2 * treeIndex + 1;
+        int rightChild = 2 * treeIndex + 2;
+
+        if (tree[leftChild].sum + acc >= target)
+        {
+            return binarySearchSum(leftChild, curLo, mid, target, acc);
+        }
+        else
+        {
+            acc += tree[leftChild].sum;
+            return binarySearchSum(rightChild, mid + 1, curHi, target, acc);
+        }
+    }
+
+    ll binarySearchMax(int treeIndex, int curLo, int curHi, int targetLo,
+                       int targetHi, ll target)
+    {
+        if (curHi < targetLo || curLo > targetHi ||
+            tree[treeIndex].maxVal < target)
+        {
+            // 当前区间与目标区间不重叠或者区间的最大值小于 target
+            return -1;
+        }
+        if (curLo == curHi)
+        {
+            // 叶节点，已经是单个元素的区间
+            return tree[treeIndex].maxVal >= target ? curLo : -1;
+        }
+
+        int mid = curLo + (curHi - curLo) / 2;
+        int leftChild = 2 * treeIndex + 1;
+        int rightChild = 2 * treeIndex + 2;
+        int res = -1;
+
+        // 先尝试在左子树中查找
+        if (mid >= targetLo)
+        { // 只有当左子树可能包含目标区间时才搜索
+            res = binarySearchMax(leftChild, curLo, mid, targetLo, targetHi,
+                                  target);
+        }
+
+        // 如果左子树没有找到，并且右子树可能包含目标区间，才在右子树中搜索
+        if (res == -1 && mid < targetHi && tree[rightChild].maxVal >= target)
+        {
+            res = binarySearchMax(rightChild, mid + 1, curHi, targetLo,
+                                  targetHi, target);
+        }
+
+        return res;
+    }
+
+    ll binarySearchMin(int treeIndex, int curLo, int curHi, int targetLo,
+                       int targetHi, ll target)
+    {
+        if (curHi < targetLo || curLo > targetHi ||
+            tree[treeIndex].minVal > target)
+        {
+            // 当前区间与目标区间不重叠或者区间的最小值大于target
+            return -1;
+        }
+        if (curLo == curHi)
+        {
+            // 叶节点，已经是单个元素的区间
+            return tree[treeIndex].minVal <= target ? curLo : -1;
+        }
+
+        int mid = curLo + (curHi - curLo) / 2;
+        int leftChild = 2 * treeIndex + 1;
+        int rightChild = 2 * treeIndex + 2;
+        int res = -1;
+
+        // 先尝试在左子树中查找
+        if (mid >= targetLo &&
+            tree[leftChild].minVal <=
+                target)
+        { // 只有当左子树可能包含目标区间时才搜索
+            res = binarySearchMin(leftChild, curLo, mid, targetLo, targetHi,
+                                  target);
+        }
+
+        // 如果左子树没有找到，并且右子树可能包含目标区间，才在右子树中搜索
+        if (res == -1 && mid < targetHi)
+        {
+            res = binarySearchMin(rightChild, mid + 1, curHi, targetLo,
+                                  targetHi, target);
+        }
+
+        return res;
+    }
+
+public:
+    SegmentTree(const vector<int> &nums)
+    {
+        n = nums.size();
+        tree.resize(4 * n);
+        buildTree(nums, 0, 0, n - 1);
+    }
+    SegmentTree()
+    {
+    }
+    // 将下标 index 的值修改为 val
+    void update(int index, ll val) { updateTree(0, 0, n - 1, index, val); }
+
+    // 查询区间 [i,j] 第一个大于等于 target 的下标
+    ll binarySearchMax(int i, int j, ll target)
+    {
+        return binarySearchMax(0, 0, n - 1, i, j, target);
+    }
+
+    // 查询区间 [i,j] 第一个小于等于 target 的下标
+    ll binarySearchMin(int i, int j, ll target)
+    {
+        return binarySearchMin(0, 0, n - 1, i, j, target);
+    }
+
+    // 查询区间 [i,j] 的最小值
+    ll queryMin(int i, int j) { return queryTree(0, 0, n - 1, i, j).minVal; }
+
+    // 查询区间 [i,j] 的最大值
+    ll queryMax(int i, int j) { return queryTree(0, 0, n - 1, i, j).maxVal; }
+
+    // 查询区间 [i,j] 的总和
+    ll querySum(int i, int j) { return queryTree(0, 0, n - 1, i, j).sum; }
+
+    // 找到第一个下标 i,使得 [0,i] 的总和大于等于 target
+    int findTargetSumIndex(ll target)
+    {
+        ll acc = 0;
+        return binarySearchSum(0, 0, n - 1, target, acc);
+    }
+};
