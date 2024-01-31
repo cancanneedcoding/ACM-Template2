141f670419e613d12553cffd14c214a1a7fb100a
zhigeng <84454978+cancanneedcoding@users.noreply.github.com>
2024-01-31 14:23:18 +0800

    Update and rename discretize.cpp to Discretize.cpp

--- Miscellaneous/Discretize.cpp
+++ Miscellaneous/Discretize.cpp
@@ -1,3 +1,5 @@
+
+// 离散化函数，将 vec 内的元素离散化后返回
 std::vector<int> discretize(const std::vector<int> &vec)
 {
     std::vector<int> sorted_vec(vec);
@@ -10,6 +12,7 @@ std::vector<int> discretize(const std::vector<int> &vec)
     result.reserve(vec.size());
     for (int v : vec)
     {
+        // 注意此处下标从 0 开始
         int discrete_value = lower_bound(sorted_vec.begin(), sorted_vec.end(), v) - sorted_vec.begin();
         result.push_back(discrete_value);
     }
