f8217d0b576f32934df7822dcae857816e4dbd5a
zhigeng <1328647034@qq.com>
2024-02-01 11:47:19 +0800

    上传对顶堆

---
+++ DataStructure/KthSmallestSumSet.cpp
@@ -0,0 +1,44 @@
+template <typename T>
+class   
+{
+public:
+    multiset<ll> low, high;
+    ll sum = 0;
+    ll k;
+    KThMinMax<T>(ll k) : k(k), sum(0)
+    {
+        low.clear();
+        high.clear();
+    }
+    void insert(ll x)
+    {
+        low.insert(x);
+        sum += x;
+        if (low.size() > k)
+        {
+            ll d = *low.rbegin();
+            sum -= d;
+            high.insert(d);
+            low.erase(low.find(d));
+        }
+    }
+    void erase(ll x)
+    {
+        if (low.find(x) != low.end())
+        {
+            sum -= x;
+            low.erase(low.find(x));
+            if (low.size() < k and high.size() > 0)
+            {
+                ll x = *high.begin();
+                high.erase(high.find(x));
+                low.insert(x);
+                sum += x;
+            }
+        }
+        else if (high.find(x) != high.end())
+        {
+            high.erase(high.find(x));
+        }
+    }
+};
---
+++ DataStructure/MedianFinders.cpp
@@ -0,0 +1,81 @@
+class MedianFinder
+{
+private:
+    multiset<int> minSet, maxSet;
+
+    // 调整两个 multiset，保持平衡
+    void balanceSets()
+    {
+        // 如果最大堆比最小堆多了两个元素，将最大堆的最大元素移动到最小堆
+        if (maxSet.size() > minSet.size() + 1)
+        {
+            auto it = prev(maxSet.end());
+            minSet.insert(*it);
+            maxSet.erase(it);
+        }
+        // 如果最小堆比最大堆多了一个元素，将最小堆的最小元素移动到最大堆
+        else if (minSet.size() > maxSet.size())
+        {
+            auto it = minSet.begin();
+            maxSet.insert(*it);
+            minSet.erase(it);
+        }
+    }
+
+public:
+    MedianFinder() {}
+
+    void clear()
+    {
+        minSet.clear();
+        maxSet.clear();
+    }
+
+    void add(int num)
+    {
+        if (maxSet.empty() || num <= *maxSet.rbegin())
+        {
+            maxSet.insert(num);
+        }
+        else
+        {
+            minSet.insert(num);
+        }
+
+        balanceSets();
+    }
+
+    double findMedian()
+    {
+        if (maxSet.size() == minSet.size())
+        {
+            return (*maxSet.rbegin() + *minSet.begin()) / 2.0; // 偶数个元素，取平均值
+        }
+        else
+        {
+            return *maxSet.rbegin(); // 奇数个元素，取最大堆的最大值
+        }
+    }
+
+    void removeNum(int num)
+    {
+        if (!maxSet.empty() && num <= *maxSet.rbegin())
+        {
+            auto it = maxSet.find(num);
+            if (it != maxSet.end())
+            {
+                maxSet.erase(it);
+            }
+        }
+        else
+        {
+            auto it = minSet.find(num);
+            if (it != minSet.end())
+            {
+                minSet.erase(it);
+            }
+        }
+
+        balanceSets();
+    }
+};
